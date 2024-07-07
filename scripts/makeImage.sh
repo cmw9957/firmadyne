0d142fdf99423e2965adc0cd1edb9faf903e4972
cmw9957 <cmw9957@gmail.com>
2024-07-08 08:16:55 +0900

    change makeImage.sh

--- scripts/makeImage.sh
+++ scripts/makeImage.sh
@@ -36,8 +36,9 @@ else
     ARCH="${ARCH#"${ARCH%%[![:space:]]*}"}"
     echo "${ARCH}"
     if [ -z "${ARCH}" ]; then
-        echo "Error: Unable to lookup architecture. Please specify {armel,mipseb,mipsel} as the second argument!"
-        exit 1
+        ARCH="mipseb"
+        #echo "Error: Unable to lookup architecture. Please specify {armel,mipseb,mipsel} as the second argument!"
+        #exit 1
     fi
 fi
 
@@ -48,29 +49,23 @@ IMAGE_DIR=`get_fs_mount ${IID}`
 CONSOLE=`get_console ${ARCH}`
 LIBNVRAM=`get_nvram ${ARCH}`
 
-echo "----Creating working directory ${WORK_DIR}----"
+echo "----Copying Filesystem Tarball----"
 mkdir -p "${WORK_DIR}"
 chmod a+rwx "${WORK_DIR}"
 chown -R "${USER}" "${WORK_DIR}"
 chgrp -R "${USER}" "${WORK_DIR}"
 
-if [ ! -e "${TARBALL_DIR}/${IID}.tar.gz" ]; then
-    echo "Error: Cannot find tarball of root filesystem for ${IID}!"
-    exit 1
+if [ ! -e "${WORK_DIR}/${IID}.tar.gz" ]; then
+    if [ ! -e "${TARBALL_DIR}/${IID}.tar.gz" ]; then
+        echo "Error: Cannot find tarball of root filesystem for ${IID}!"
+        exit 1
+    else
+        cp "${TARBALL_DIR}/${IID}.tar.gz" "${WORK_DIR}/${IID}.tar.gz"
+    fi
 fi
 
-
-TARBALL_SIZE=$(tar ztvf "${TARBALL_DIR}/${IID}.tar.gz" --totals 2>&1 |tail -1|cut -f4 -d' ')
-MINIMUM_IMAGE_SIZE=$((TARBALL_SIZE + 10 * 1024 * 1024))
-echo "----The size of root filesystem '${TARBALL_DIR}/${IID}.tar.gz' is $TARBALL_SIZE-----"
-IMAGE_SIZE=8388608
-while [ $IMAGE_SIZE -le $MINIMUM_IMAGE_SIZE ]
-do
-    IMAGE_SIZE=$((IMAGE_SIZE*2))
-done
-
-echo "----Creating QEMU Image ${IMAGE} with size ${IMAGE_SIZE}----"
-qemu-img create -f raw "${IMAGE}" $IMAGE_SIZE
+echo "----Creating QEMU Image----"
+qemu-img create -f raw "${IMAGE}" 1G
 chmod a+rw "${IMAGE}"
 
 echo "----Creating Partition Table----"
@@ -79,13 +74,12 @@ echo -e "o\nn\np\n1\n\n\nw" | /sbin/fdisk "${IMAGE}"
 echo "----Mounting QEMU Image----"
 DEVICE=$(get_device "$(kpartx -a -s -v "${IMAGE}")")
 sleep 1
-echo "----Device mapper created at ${DEVICE}----"
 
 echo "----Creating Filesystem----"
 mkfs.ext2 "${DEVICE}"
 sync
 
-echo "----Making QEMU Image Mountpoint at ${IMAGE_DIR}----"
+echo "----Making QEMU Image Mountpoint----"
 if [ ! -e "${IMAGE_DIR}" ]; then
     mkdir "${IMAGE_DIR}"
     chown "${USER}" "${IMAGE_DIR}"
@@ -94,24 +88,35 @@ fi
 echo "----Mounting QEMU Image Partition 1----"
 mount "${DEVICE}" "${IMAGE_DIR}"
 
-echo "----Extracting Filesystem Tarball to Mountpoint----"
-tar -xf "${TARBALL_DIR}/${IID}.tar.gz" -C "${IMAGE_DIR}"
+echo "----Extracting Filesystem Tarball----"
+tar -xf "${WORK_DIR}/$IID.tar.gz" -C "${IMAGE_DIR}"
+rm "${WORK_DIR}/${IID}.tar.gz"
 
 echo "----Creating FIRMADYNE Directories----"
 mkdir "${IMAGE_DIR}/firmadyne/"
+#mkdir "${IMAGE_DIR}/FILE_LOAD/"
 mkdir "${IMAGE_DIR}/firmadyne/libnvram/"
 mkdir "${IMAGE_DIR}/firmadyne/libnvram.override/"
 
 echo "----Patching Filesystem (chroot)----"
 cp $(which busybox) "${IMAGE_DIR}"
 cp "${SCRIPT_DIR}/fixImage.sh" "${IMAGE_DIR}"
+#cp  -a "${FIRMWARE_DIR}/FILE_LOAD/." "${IMAGE_DIR}/FILE_LOAD/"
 chroot "${IMAGE_DIR}" /busybox ash /fixImage.sh
 rm "${IMAGE_DIR}/fixImage.sh"
 rm "${IMAGE_DIR}/busybox"
 
+#rm "${IMAGE_DIR}/usr/sbin/gpiod" #9054
+#rm "${IMAGE_DIR}/etc/init.d/S45gpiod.sh" #9050 dlink
+#cp "${IMAGE_DIR}/userfs/bin/tcapi" "${IMAGE_DIR}/userfs/bin/tcapi_new" #105609 DSL-3782
+#rm "${IMAGE_DIR}/usr/sbin/fresetd" #9925
+#cp "../firmadyne_modify/webs_9925.sh" "${IMAGE_DIR}/etc/templates/webs.sh" #9925
+#cp "../firmadyne_modify/http-loop_9925.sh" "${IMAGE_DIR}/http-loop.sh" #9925
+
 echo "----Setting up FIRMADYNE----"
 cp "${CONSOLE}" "${IMAGE_DIR}/firmadyne/console"
 chmod a+x "${IMAGE_DIR}/firmadyne/console"
+#chmod -R a+x "${IMAGE_DIR}/FILE_LOAD/"
 mknod -m 666 "${IMAGE_DIR}/firmadyne/ttyS1" c 4 65
 
 cp "${LIBNVRAM}" "${IMAGE_DIR}/firmadyne/libnvram.so"
@@ -120,10 +125,13 @@ chmod a+x "${IMAGE_DIR}/firmadyne/libnvram.so"
 cp "${SCRIPT_DIR}/preInit.sh" "${IMAGE_DIR}/firmadyne/preInit.sh"
 chmod a+x "${IMAGE_DIR}/firmadyne/preInit.sh"
 
+echo "${IMAGE_DIR}"
+echo "${FIRMWARE_DIR}/../image_${IID}"
+cp -r "${IMAGE_DIR}" "${FIRMWARE_DIR}/../image_${IID}" #zyw
+
 echo "----Unmounting QEMU Image----"
 sync
 umount "${DEVICE}"
-echo "----Deleting device mapper----"
 kpartx -d "${IMAGE}"
 losetup -d "${DEVICE}" &>/dev/null
 dmsetup remove $(basename "$DEVICE") &>/dev/null
